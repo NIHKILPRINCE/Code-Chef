b0c7c2e6e2339da447de5ad83d4212e4ecd73f2a
Nikhil Prince Sodadasi <166209449+NIHKILPRINCE@users.noreply.github.com>
2024-10-21 16:02:59 +0530

    Add files via upload

---
+++ all list functions.py
@@ -0,0 +1,66 @@
+python lists methods or operations:
+----------------------------------
+1.append():
+ syntax:
+ list_name.append(element)
+ it is used for adding elements to the end of the list
+2.insert():
+ syntax:
+ list_name.insert(position,element)
+ it is used to insert element in certain position.
+3.extend():
+ syntax:
+ list_1name.extend(list_2)
+ it is used to extend the list that means concatenating the lists
+4.sum():
+ syntax:
+ sum(list_name)
+ it is used to add the elements of list
+5.count():
+ syntax:
+ list_name.count(element)
+ it is used to find count of element in that list
+6.len():
+ syntax:
+ len(list_name)
+ it is used for knowing length of list
+7.index():
+ syntax:
+ list_name.index(element)
+ it is used for knowing the element index
+8.min():
+ syntax:
+ min(list_name)
+ it is used for finding minimum element in the
+9.max():
+ syntax:
+ max(list_name)
+ it is used for finding the max number of list
+10.sort():
+ syntax:
+ list_name.sort()
+ it used for setting elements in sorted oder
+11.pop():
+ syntax_1:
+ list.pop()
+ in this type of syntax last will be removed from list
+ syntax_2:
+ list.pop(index)
+ in this we delete the element we want using index
+12.del():
+ syntax_1:
+ del list_name[index]
+ in this only that index element is deleted
+ syntax_2:
+ del list_name[start_index:last_index]
+ in this we delete elements from start to end index
+13.remove():
+ syntax:
+ list.remove(element)
+ it is used for remove specific element using it value/name.
+14.copy():
+ syntax:
+ list_2name=list_1name.copy()
+ it is used for copying elements from one list to another list
+
+ 
