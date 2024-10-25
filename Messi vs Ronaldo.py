603cab236922e18a07ce8c3c13b38c62a152f2cd
Nikhil Prince Sodadasi <166209449+NIHKILPRINCE@users.noreply.github.com>
2024-10-25 18:02:44 +0530

    Create Messi vs Ronaldo.py

---
+++ Messi vs Ronaldo.py
@@ -0,0 +1,8 @@
+# cook your dish here
+a,b,c,d=map(int,input().split())
+if((a*2+b)>(c*2+d)):
+    print("Messi")
+if((a*2+b)<(c*2+d)):
+    print("Ronaldo")
+if((a*2+b)==(c*2+d)):
+    print("Equal")
