87fde57db5b9172bab81d92e3c77385165b11f57
Nikhil Prince Sodadasi <166209449+S-Nikhil-Prince@users.noreply.github.com>
2024-11-07 20:09:10 +0530

    Create Donations Rewards.py

---
+++ Donations Rewards.py
@@ -0,0 +1,9 @@
+# cook your dish here
+for _ in range(int(input())):
+    a=int(input())
+    if(a<=3):
+        print("BRONZE")
+    if(a>3 and a<=6 ):
+        print("SILVER")
+    if(a>6):
+        print("GOLD")
